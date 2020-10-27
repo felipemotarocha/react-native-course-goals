4b17a64b3524f4a7aae9936a475c0f5fa2d26af6
Felipe Rocha <felipemota.rocha@gmail.com>
2020-10-27 17:53:43 -0300

    feat: custom fonts

--- App.tsx
+++ App.tsx
@@ -4,10 +4,30 @@ import Header from "./components/Header";
 import GameOverScreen from "./screens/GameOverScreen";
 import GameScreen from "./screens/GameScreen";
 import StartGameScreen from "./screens/StartGameScreen";
+import * as Font from "expo-font";
+import { AppLoading } from "expo";
+
+const fetchFonts = () => {
+	return Font.loadAsync({
+		"open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+		"open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+	});
+};
 
 export default function App() {
 	const [userNumber, setUserNumber] = useState<number | null>();
 	const [guessRounds, setGuessRounds] = useState(0);
+	const [dataLoaded, setDataLoaded] = useState(false);
+
+	if (!dataLoaded) {
+		return (
+			<AppLoading
+				startAsync={fetchFonts}
+				onFinish={() => setDataLoaded(true)}
+				onError={(err) => console.log(err)}
+			/>
+		);
+	}
 
 	const handleConfigureNewGame = () => {
 		setGuessRounds(0);
